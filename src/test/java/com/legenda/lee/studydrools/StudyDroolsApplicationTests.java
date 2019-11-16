9213b6be68f763269303559f3f8a122882316571
Legenda-Lee <lee.legenda@gmail.com>
2019-11-16 21:08:03 +0800

    init

---
+++ src/test/java/com/legenda/lee/studydrools/StudyDroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legenda.lee.studydrools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDroolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
